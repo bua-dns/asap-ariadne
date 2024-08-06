b6e74e56e01f05591dc082e9c8a8fa9cee8a764d
culturetogo <m.mueller@culture-to-go.de>
2024-08-06 11:00:39 +0200

    captions on exhibition start page

--- src/components/parts/ContentBlock.vue
+++ src/components/parts/ContentBlock.vue
@@ -9,6 +9,7 @@ const props = defineProps({
 
 <template>
     <div class="block-element">
+        <div class="dev-out" v-if="false">{{ blockContent.caption }}</div>
         <template v-if="props.blockContent._block === 'textsection'">
             <div class="text-section">
                 <div class="content" v-html="props.blockContent.content" />
@@ -16,8 +17,11 @@ const props = defineProps({
         </template>
         <template v-if="props.blockContent._block === 'textwithimagesection'">
             <div class="block-container">
-                <div class="block-image">
-                    <img :src="useGetDerivate(props.blockContent.image.url, '800', '800')" alt="">
+                <div class="block-image-container">
+                    <div class="block-image">
+                        <img :src="useGetDerivate(props.blockContent.image.url, '800', '800')" alt="">
+                        <div class="caption" v-if="blockContent.caption" v-html="blockContent.caption" />
+                    </div>
                 </div>
                 <div class="block-text">
                     <div class="content" v-html="props.blockContent.content" />
@@ -25,7 +29,10 @@ const props = defineProps({
             </div>
         </template>
         <template v-if="props.blockContent._block === 'imagesection'">
-            <img :src="props.blockContent.image.url" alt="">
+            <div class="block-image">
+                <img :src="props.blockContent.image.url" alt=""> 
+                <div class="caption" v-if="blockContent.caption" v-html="blockContent.caption" />
+            </div>
         </template>     
     </div>
 </template>
@@ -51,15 +58,22 @@ const props = defineProps({
         display: block;
 
         padding: 1rem;
-        .block-image {
+        .block-image-container {
             display: flex;
             justify-content: center;
             align-items: flex-start;
             padding: 1rem;
-            img {
-                display: block;
-                max-width: 100%;
-                max-height: calc(100vh - 320px);
+            .block-image {
+                img {
+                    display: block;
+                    max-width: 100%;
+                    max-height: calc(100vh - 320px);
+                }
+                .caption {
+                    font-size: var(--font-size-extra-small);
+                    text-align: center;
+                    padding: 1rem;
+                }
             }
         }
         .block-text {
@@ -73,29 +87,7 @@ const props = defineProps({
         }
     }
 }
-@media (min-width: 480px) {
-    .block-element {
 
-        .text-section {
-
-        }
-        .block-container {
-
-            .block-image {
-
-                img {
-
-                }
-            }
-            .block-text {
-
-                .content {
-                    
-                }
-            }
-        }
-    }
-}
 @media (min-width: 768px) {
         .block-element {
 
@@ -107,68 +99,7 @@ const props = defineProps({
             grid-template-columns: 1fr 1fr;
             gap: 5rem;
             padding: 2rem;
-            .block-image {
-
-                img {
-
-                }
-            }
-            .block-text {
-
-                .content {
-                    
-                }
-            }
         }
     }
 }
-@media (min-width: 1024px) {
-        .block-element {
-
-        .text-section {
-
-        }
-        .block-container {
-
-            .block-image {
-
-                img {
-
-                }
-            }
-            .block-text {
-
-                .content {
-                    
-                }
-            }
-        }
-    }
-}
-@media (min-width: 1200px) {
-    .block-element {
-
-        .text-section {
-
-        }
-        .block-container {
-
-            .block-image {
-
-                img {
-
-                }
-            }
-            .block-text {
-
-                .content {
-                    
-                }
-            }
-        }
-    }
-}
-
-
-
 </style>
